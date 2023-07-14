8ad4ca0ba58756a62b105eb4497f6e24d8ed8354
NdiranguAndrew43 <andymurunga@gmail.com>
2023-07-14 07:13:27 -0700

    added 7-print_tebahpla.c and betty checked also gcc..

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: eachtime 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
