9596fa63dc0a140abeba9f346c4693929ef9155f
NdiranguAndrew43 <andymurunga@gmail.com>
2023-07-16 02:58:40 -0700

    added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints "and that piece of  of art is useful" - Dora Korper, 2015-10-19",
- * followed by a newline, to standard error.
+ * main - Entry
  * Return: Always 1
  */
-
-
 int main(void)
 {
-	printf(2, "and that piece of art is useful\" -Dora korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" -Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
