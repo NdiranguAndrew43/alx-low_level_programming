a19f91cd5f6e3b45f5ccdccf22725b5f0fadeebc
NdiranguAndrew43 <andymurunga@gmail.com>
2023-07-20 07:09:09 -0700

    added the file 1-isdigit with nessesary checks in betty and compiler

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit in the given range
+ * @c: the digit being checked for
+ * Return: alwasy 1 if c is a digit and 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
