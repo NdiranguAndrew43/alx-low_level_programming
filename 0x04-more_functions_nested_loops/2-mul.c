82ba5b8005de930e81ec51f448dcfc9a76792c2a
NdiranguAndrew43 <andymurunga@gmail.com>
2023-07-20 07:43:43 -0700

    added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - code to multiply two random integers
+ * @a: first random integer to be multiplied by second random integer
+ * @b: second random integer to be multiplied by first random integer
+ * Return: the result of multiplication between param a and param b
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
+
