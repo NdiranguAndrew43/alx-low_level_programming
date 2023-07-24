b14b8cc2683977aed8c565f6616ab2b54944257d
NdiranguAndrew43 <andymurunga@gmail.com>
2023-07-24 02:02:37 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that takes pointer to an int
+ * and updates the value to to 98
+* @n: an integer input pointer
+* Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+/*These are reference functions: as prototypes for the nessesary functions.
+ */
+
+void reset_to_98(int *n);
