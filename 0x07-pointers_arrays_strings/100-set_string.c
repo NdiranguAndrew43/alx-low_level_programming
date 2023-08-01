64f32746a4e9b129969e5aec1f21ea44063044a6
NdiranguAndrew43 <andymurunga@gmail.com>
2023-08-01 03:08:45 -0700

    added proper 100-set..

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,13 +2,13 @@
 
 /**
  * set_string - entry point
- * @s: input 
- * @to: input 
+ * @s: input
+ * @to: input
  * Return: always 0 (success)
  */
 
 void set_string(char **s, char *to)
 
 {
-	**s = *to;
+	*s = to;
 }
