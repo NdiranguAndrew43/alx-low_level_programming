5a692fe569cccd14ba77b408746cf96996160d94
NdiranguAndrew43 <andymurunga@gmail.com>
2023-08-01 00:03:30 -0700

    all added and hopeful

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - entry point
+ * @a: array
+ * Return: always 0 (success)
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	unsigned int i;
+	unsigned int n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			_putchar(a[i][n]);
+		}
+		_putchar('\n');
+	}
+}
